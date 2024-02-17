310ceaa2bb546fcceb26cc226623573c29aff95d
Sahil Hashimli <hashimlisahil@gmail.com>
2024-02-17 17:55:25 +0100

    Added list tags endpoint

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, \
                                         PermissionsMixin
-# Create your models here.
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
@@ -39,3 +39,16 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Tag(models.Model):
+
+    """Tag model that is used for the recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
