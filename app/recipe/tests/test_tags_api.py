0901cc37893554afce8849ba3c19b0e37e9ab944
Sahil Hashimli <hashimlisahil@gmail.com>
2024-02-17 18:08:43 +0100

    Added create a new tag feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -72,3 +72,27 @@ class PrivateagApiTests(TestCase):
         self.assertEqual(len(res.data), 1)
         # Test that names (Meat) are equal
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successfully(self):
+        """Test that a new tag is created successfully"""
+        payload = {
+            'name': 'Test tag'
+        }
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test that creating new tag with invalid payload"""
+        payload = {
+            'name': ''
+        }
+
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
