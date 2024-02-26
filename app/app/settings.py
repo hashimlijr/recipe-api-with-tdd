7fcc1c3a0807e385a7f00b531c22ae8555ac5699
Sahil Hashimli <hashimlisahil@gmail.com>
2024-02-26 22:16:12 +0100

    Pillow and image field are added

--- app/app/settings.py
+++ app/app/settings.py
@@ -125,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+STATIC_ROOT = 'vol/web/static'
+MEDIA_ROOT = 'vol/web/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -174,3 +174,47 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(ingredients.count(), 2)
         self.assertIn(ingredients1, ingredients)
         self.assertIn(ingredients2, ingredients)
+
+    def test_partial_update_recipe(self):
+        """Test update recipe with patch"""
+
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tags(user=self.user))
+        new_tag = sample_tags(user=self.user, name='Fast food')
+
+        payload = {
+            'title': 'Pizza',
+            'tags': [new_tag.id]
+        }
+
+        url = detail_url(recipe_id=recipe.id)
+        self.client.patch(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 1)
+        self.assertIn(new_tag, tags)
+
+    def test_full_update_recipe(self):
+        """Test update recipe with put"""
+
+        recipe = sample_recipe(user=self.user)
+        recipe.tags.add(sample_tags(user=self.user))
+
+        payload = {
+            'title': 'Pizza',
+            'time_minutes': 20,
+            'price': 10.00,
+        }
+
+        url = detail_url(recipe_id=recipe.id)
+        self.client.put(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.title, payload['title'])
+        self.assertEqual(recipe.time_minutes, payload['time_minutes'])
+        self.assertEqual(recipe.price, payload['price'])
+
+        tags = recipe.tags.all()
+        self.assertEqual(len(tags), 0)
